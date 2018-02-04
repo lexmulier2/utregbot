6355ac9f77f878421f84d094d3b1f972c4b0799f
Lex Mulier <lexmulier@gmail.com>
2018-02-04 16:18:30 +0100

    scores

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,18 +8,22 @@ class Game(object):
     def __init__(self, team=None):
         self.url = 'http://www.soccernews.nl/livescore/'
 
-        self.team = team or 'FC utrecht'
+        self.team = team or 'FC Utrecht'
         self.team_lower = self.team.replace(' ', '_').lower()
 
         self.game_url = None
+        self.competition = None
         self.at_home = None
         self.opponent = None
+        self.time = None
+        self.scores = []
 
     def check_game_today(self):
         soup = self.get_soup(self.url)
         if soup:
-            for g in soup.find_all('div', class_='livescore__result'):
+            for g in soup.find_all('div', class_='livescore livescore--off'):
                 if self.team_lower in g.find('a')['href']:
+                    self.competition = g.find_previous_sibling('h3').text.split(' - ')[0].strip()
                     self.game_url = self.url + g.find('a')['href']
                     return True
         return False
@@ -31,7 +37,39 @@ class Game(object):
             self.opponent = teams[1] if self.at_home else teams[0]
 
             # Time
+            raw_time = list(map(int, soup.find('time', class_='score__date').text.split(' om ')[1].split(':')))
+            self.time = datetime.datetime.now().replace(hour=raw_time[0], minute=raw_time[1], second=0)
 
+    def notify_game_today(self):
+        game_time = self.time.strftime('%H:%M')
+        location = 'thuis' if self.at_home else 'uit'
+        message_template = "Ons cluppie moet vandaag om {} {} voetballen tegen {} ({})"
+        message = message_template.format(game_time, location, self.opponent, self.competition)
+        return message
+
+    def get_score(self):
+        soup = self.get_soup(self.game_url)
+        if soup:
+            for event in soup.find_all('div', class_='livescore'):
+                goal = event.find('div', class_='livescore__stand')
+                if len(goal.text) and goal.text not in self.scores:
+                    self.scores.append(goal.text)
+                    time = event.find('div', class_='livescore__position').text.strip()
+                    player = event.find('strong')
+                    player_name = event.find('strong').text.strip()
+                    our_team = ('livescore__player' in player.parent.get('class', '')) == self.at_home
+                    if our_team:
+                        return self.goal_for_us(goal.text, time, player_name)
+                    else:
+                        return self.goal_for_them(goal.text, time, player_name)
+
+    def goal_for_us(self, goal, time, player):
+        template = 'UUUUUUUU!!!! {} scoort voor {} en de stand is nu {} ({}). UUUUUU!!!'
+        return template.format(player, self.team, goal, time)
+
+    def goal_for_them(self, goal, time, player):
+        template = 'NEEEE!!!! {} scoort voor {} en de stand is nu {} ({}). SHEMALES!!!'
+        return template.format(player, self.opponent, goal, time)
 
     @staticmethod
     def get_soup(url):
@@ -40,8 +78,8 @@ class Game(object):
             return BeautifulSoup(html.text, 'html.parser')
         return None
 
-
 if __name__ == '__main__':
     game = Game()
     if game.check_game_today():
         game.get_stats()
+        message = game.get_score()
--- main.py
+++ main.py
@@ -1,27 +1,51 @@
+import datetime
+
 from telegram.ext import Updater, CommandHandler
+
+from game import Game
 
 TOKEN = '520822004:AAFKK9EozBuM-GUHuxhxSV9v5FdLFq-D35E'
 
 
-def next_game(bot, update):
-    update.message.reply_text('Hi!')
+class UtregBot(object):
+    def __init__(self, chat_id=None):
+        self.chat_id = chat_id or '-16383268'
+        self.game = None
+
+        self.updater = Updater(TOKEN)
+        self.dp = self.updater.dispatcher
+
+        # Jobs handler
+        self.jobs = self.updater.job_queue
+        self.jobs.run_daily(self.get_game, datetime.time(hour=15, minute=28))
+        self.jobs.run_repeating(self.check_score, interval=60)
+
+        self.dp.add_handler(CommandHandler('uuu', self.uuu))
+
+        self.updater.start_polling()
+
+        self.updater.idle()
+
+    def get_game(self, bot, update):
+        self.game = Game()
+        if self.game.check_game_today():
+            self.game.get_stats()
+            message = self.game.notify_game_today()
+            bot.send_message(chat_id=self.chat_id, text=message)
+
+    def check_score(self, bot, update):
+        current_time = datetime.datetime.now()
+        if self.game < current_time < self.game.timedelta(hours=3):
+            if self.game.get_score():
+                message = self.game.score_message()
+                print(message)
+                #bot.send_message(chat_id=self.chat_id, text=message)
+
+    @staticmethod
+    def uuu(bot, update):
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id=chat_id, text='UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU')
 
 
-def uuu(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text='UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU')
-
-
-def main():
-
-    game = current_game()
-    # updater = Updater(TOKEN)
-    # dp = updater.dispatcher
-    # dp.add_handler(CommandHandler('nextgame', next_game))
-    # dp.add_handler(CommandHandler('uuu', uuu))
-    #
-    # updater.start_polling()
-    #
-    # updater.idle()
-    #
-
+if __name__ == '__main__':
+    UtregBot()
